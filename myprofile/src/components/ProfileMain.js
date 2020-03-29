30df62286808c10717c416630700a645af1d13fb
Tanaya <tanaya@Tanayas-MacBook-Pro.local>
2020-03-29 02:16:58 -0400

    :pencil2: adding image

--- myprofile/src/components/ProfileMain.js
+++ myprofile/src/components/ProfileMain.js
@@ -1,10 +1,10 @@
 import React from 'react';
+import '../styles/components/profileMain.scss';
 
 
 export default function ProfileMain() {
+
     return(
-        <div>
-            
-        </div>
+        <div className="backgroundimage"></div>
     );
 }
